9aea3c0684d1193a79996c9ccaec6fa2eafd7597
gkrishna4346 <gkrishna.4346@gmail.com>
2026-07-08 16:42:45 +0530

    Initial commit

---
+++ table.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Multiplication Table")
+
+number = st.number_input("Enter the number:", value=None, step=1)
+
+if st.button("Generate Table"):
+    st.subheader(f"Multiplication Table of {int(number)}")
+
+    for i in range(1, 21):
+        st.write(f"{i} x {int(number)} = {i * int(number)}")
