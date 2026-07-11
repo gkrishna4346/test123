e33326c117be7a0b0872da653bfde0b13febc943
gkrishna4346 <gkrishna.4346@gmail.com>
2026-07-11 14:09:43 +0530

    Update table.py

--- table.py
+++ table.py
@@ -5,7 +5,7 @@ st.title("Multiplication Table - GK")
 number = st.number_input("Enter the number:", value=None, step=1)
 
 if st.button("Generate Table"):
-    st.subheader(f"Multiplication Table of {int(number)}")
+    st.subheader(f"GK - Multiplication Table of {int(number)}")
 
     for i in range(1, 21):
         st.write(f"{i} x {int(number)} = {i * int(number)}")
